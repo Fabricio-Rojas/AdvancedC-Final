60e6009ef373c97fe7288b7c09283b4a326aa0c1
Fabricio Rojas <fabriciovmamanir@gmail.com>
2023-09-20 09:45:18 -0500

    Add project files.

---
+++ AdvancedC#Final/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdvancedC_Final.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
